c3618b3860c0d6040fb18a9759368f1a4548ffa2
Benjamin van der Wolf <Vanderwolf7@gmail.com>
2026-05-26 17:03:06 +0200

    Update pooling api with list change event and exposed list

--- Runtime/Pooling/PoolGameObject.cs
+++ Runtime/Pooling/PoolGameObject.cs
@@ -2,5 +2,10 @@ namespace OpenUtility.Data.Pooling
 {
     public class PoolGameObject : PoolGameObjectBase<PoolGameObject>
     {
+        /// <summary>
+        /// The list of pooled game objects this game object is part of. Will return null if this component is
+        /// not part of a pool or if the pool has no references.
+        /// </summary>
+        public PoolGameObjectList List => pool == null ? null : ((ScriptablePool)pool).References;
     }
 }
--- Runtime/Pooling/PoolGameObjectList.cs
+++ Runtime/Pooling/PoolGameObjectList.cs
@@ -1,11 +1,62 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace OpenUtility.Data.Pooling
 {
+    public enum ListChangeEvent
+    {
+        /// <summary>
+        /// An entry has been added to the list. Expected contextual argument should be the new entry.
+        /// </summary>
+        ADDITION,
+        
+        /// <summary>
+        /// An entry has been removed from the list. Expected contextual argument should be the removed entry.
+        /// </summary>
+        REMOVAL,
+        
+        /// <summary>
+        /// The list has been cleared. Expected contextual argument should be null.
+        /// </summary>
+        CLEARED
+    }
+    
     [CreateAssetMenu(fileName = "PoolGameObjectList", menuName = "OpenUtility/Pooling/GameObject List", order = 1)]
     public class PoolGameObjectList : ScriptableList<PoolGameObject>
     {
+        public event Action<ListChangeEvent, PoolGameObject> Changed;
+
+        public override void Add(PoolGameObject newValue)
+        {
+            base.Add(newValue);
+            
+            Changed?.Invoke(ListChangeEvent.ADDITION, newValue);
+        }
+
+        public override void Remove(PoolGameObject valueToRemove)
+        {
+            base.Remove(valueToRemove);
+            
+            Changed?.Invoke(ListChangeEvent.REMOVAL, valueToRemove);
+        }
+
+        public override void RemoveAt(int index)
+        {
+            PoolGameObject instance = GetValue(index);
+            
+            base.RemoveAt(index);
+            
+            Changed?.Invoke(ListChangeEvent.REMOVAL, instance);
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+            
+            Changed?.Invoke(ListChangeEvent.CLEARED, null);
+        }
+
         /// <summary>
         /// Releases all instances in this list.
         /// </summary>
