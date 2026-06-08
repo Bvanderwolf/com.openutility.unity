013223af14a55b6ab4320503c274df77c350e79a
Benjamin van der Wolf <Vanderwolf7@gmail.com>
2026-06-08 17:25:35 +0200

    Update PoolSpawnArea with spawn count and spawn delay.

--- Runtime/Pooling/PoolSpawnArea.cs
+++ Runtime/Pooling/PoolSpawnArea.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace OpenUtility.Data.Pooling
@@ -12,15 +13,54 @@ namespace OpenUtility.Data.Pooling
         [SerializeField]
         private Optional<Transform> _parent;
 
+        [Header("Settings")]
+        [SerializeField]
+        private IntReference _spawnCount;
+
+        [SerializeField]
+        private FloatReference _spawnDelay;
+
+        private Coroutine _spawnRoutine;
+
         private void Awake()
         {
             if (_parent.HasValue)
                 _pool.SetParent(_parent.Value);
         }
 
-        public void Spawn()
+        public void Spawn() => Spawn(_spawnCount, _spawnDelay);
+
+        public void Spawn(int spawnCount, float spawnDelay)
         {
-            _pool.Get();
+            if (_spawnRoutine != null)
+                StopCoroutine(_spawnRoutine);
+            
+            if (spawnCount > 1 && spawnDelay > 0f)
+            {
+                _spawnRoutine = StartCoroutine(SpawnRoutine(spawnCount, spawnDelay));
+            }
+            else
+            {
+                for (int i = 0; i < spawnCount; i++)
+                    _pool.Get();
+            }
+        }
+
+        private IEnumerator SpawnRoutine(int amount, float delay)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                _pool.Get();
+
+                float endTime = Time.time + delay;
+                while (Time.time < endTime)
+                {
+                    if (Application.exitCancellationToken.IsCancellationRequested)
+                        yield break;
+
+                    yield return null;
+                }
+            }
         }
     }
 }
