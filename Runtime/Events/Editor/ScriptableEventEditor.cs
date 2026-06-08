4d2152d03a6ad568a9b0b0b3f1509c6804144c01
Benjamin van der Wolf <Vanderwolf7@gmail.com>
2026-06-08 11:11:12 +0200

    Add editor for ScriptableEvent

---
+++ Runtime/Events/Editor/ScriptableEventEditor.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace OpenUtility.Data.Events.Editor
+{
+    [CustomEditor(typeof(ScriptableEvent))]
+    public class ScriptableEventEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            if (GUILayout.Button("Invoke"))
+                ((ScriptableEvent)target).Invoke();
+        }
+    }
+}
