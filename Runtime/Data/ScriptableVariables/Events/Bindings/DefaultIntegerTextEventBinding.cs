4ca1218c55a10361b5a7631b41dea5f926ef99b1
Benjamin van der Wolf <Vanderwolf7@gmail.com>
2026-07-27 09:50:44 +0200

    Update DefaultIntegerTextEventBinding with optional formatting

--- Runtime/Data/ScriptableVariables/Events/Bindings/DefaultIntegerTextEventBinding.cs
+++ Runtime/Data/ScriptableVariables/Events/Bindings/DefaultIntegerTextEventBinding.cs
@@ -1,10 +1,34 @@
+using System;
+using System.Globalization;
 using TMPro;
+using UnityEngine;
 
 namespace OpenUtility.Data
 {
     [ScriptableVariableBinder(typeof(TMP_Text), typeof(int), BindingGoal.DetermineValue, DisplayName = "Default Integer")]
     public class DefaultIntegerTextEventBinding : IntegerTextEventBinding
     {
-        protected override string ConvertIntegerToText(int newValue) => newValue.ToString();
+        [Header("Optional")]
+        [SerializeField]
+        private Optional<string> _intFormat;
+
+        [SerializeField]
+        private Optional<string> _stringFormat;
+
+        protected override string ConvertIntegerToText(int newValue)
+        {
+            string text = _intFormat.HasValue
+                ? newValue.ToString(_intFormat.Value)
+                : newValue.ToString(CultureInfo.InvariantCulture);
+            
+            try
+            {
+                return (_stringFormat.HasValue ? string.Format(_stringFormat.Value, text) : text);
+            }
+            catch (Exception)
+            {
+                return (text);
+            }
+        }
     }
 }
