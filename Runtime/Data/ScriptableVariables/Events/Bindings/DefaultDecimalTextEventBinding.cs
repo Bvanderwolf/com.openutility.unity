44e8e537c46307665d4c4ed79653f4b713c8f5c4
Benjamin van der Wolf <Vanderwolf7@gmail.com>
2026-07-27 10:46:39 +0200

    Update Default text event bindings for float and double with formats

--- Runtime/Data/ScriptableVariables/Events/Bindings/DefaultDecimalTextEventBinding.cs
+++ Runtime/Data/ScriptableVariables/Events/Bindings/DefaultDecimalTextEventBinding.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 using TMPro;
 using UnityEngine;
@@ -9,11 +10,25 @@ namespace OpenUtility.Data
     {
         [Header("Optional")]
         [SerializeField]
-        private Optional<string> _format;
+        private Optional<string> _floatFormat;
+        
+        [SerializeField]
+        private Optional<string> _stringFormat;
         
         protected override string ConvertDecimalToText(float newValue)
         {
-            return (_format.HasValue ? newValue.ToString(_format.Value) : newValue.ToString(CultureInfo.InvariantCulture));
+            string text = _floatFormat.HasValue
+                ? newValue.ToString(_floatFormat.Value)
+                : newValue.ToString(CultureInfo.InvariantCulture);
+
+            try
+            {
+                return (_stringFormat.TryGetValue(out string format) ? string.Format(format, text) : text);
+            }
+            catch (Exception)
+            {
+                return (text);
+            }
         }
     }
 }
--- Runtime/Data/ScriptableVariables/Events/Bindings/DefaultPreciseDecimalTextEventBinding.cs
+++ Runtime/Data/ScriptableVariables/Events/Bindings/DefaultPreciseDecimalTextEventBinding.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 using TMPro;
 using UnityEngine;
@@ -7,12 +8,27 @@ namespace OpenUtility.Data
     [ScriptableVariableBinder(typeof(TMP_Text), typeof(double), BindingGoal.DetermineValue, DisplayName = "Default Precise Decimal")]
     public class DefaultPreciseDecimalTextEventBinding : PreciseDecimalTextEventBinding
     {
+        [Header("Optional")]
         [SerializeField]
-        private Optional<string> _format;
+        private Optional<string> _doubleFormat;
+        
+        [SerializeField]
+        private Optional<string> _stringFormat;
         
         protected override string ConvertDecimalToText(double newValue)
         {
-            return (_format.HasValue ? newValue.ToString(_format.Value) : newValue.ToString(CultureInfo.InvariantCulture));
+            string text = _doubleFormat.HasValue
+                ? newValue.ToString(_doubleFormat.Value)
+                : newValue.ToString(CultureInfo.InvariantCulture);
+
+            try
+            {
+                return (_stringFormat.TryGetValue(out string format) ? string.Format(format, text) : text);
+            }
+            catch (Exception)
+            {
+                return (text);
+            }
         }
     }
 }
