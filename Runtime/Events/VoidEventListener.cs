09fe34ae75eb8c422f7432d68774a1ce0d2308f6
Benjamin van der Wolf <Vanderwolf7@gmail.com>
2026-07-29 11:50:04 +0200

    Add TypedEventListener class

--- Runtime/Events/VoidEventListener.cs
+++ Runtime/Events/VoidEventListener.cs
@@ -1,8 +1,40 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace OpenUtility.Data.Events
 {
+    public abstract class TypedEventListener<T> : MonoBehaviour
+    {
+        [Serializable]
+        private class Event : UnityEvent<T> {}
+        
+        [Header("Project References")]
+        [SerializeField]
+        private ScriptableEvent<T> _event;
+
+        [SerializeField]
+        private Event _onEvent;
+        
+        private void OnEnable()
+        {
+            _event.AddListener(OnEvent);
+        }
+        
+        private void OnDisable()
+        {
+            _event.RemoveListener(OnEvent);
+        }
+
+        private void OnEvent(T value)
+        {
+            if (Application.exitCancellationToken.IsCancellationRequested)
+                return;
+            
+            _onEvent?.Invoke(value);
+        }
+    }
+    
     public class VoidEventListener : MonoBehaviour
     {
         [Header("Project References")]
