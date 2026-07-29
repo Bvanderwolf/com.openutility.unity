b56faa900f8ef515a72e6bfc63b28a9d1dfd5427
Benjamin van der Wolf <Vanderwolf7@gmail.com>
2026-07-29 11:49:43 +0200

    Add generic ScriptableEvent class

--- Runtime/Events/ScriptableEvent.cs
+++ Runtime/Events/ScriptableEvent.cs
@@ -1,8 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace OpenUtility.Data.Events
 {
+    public abstract class ScriptableEvent<T> : ScriptableObject
+    {
+        [Serializable]
+        private class Event : UnityEvent<T> {}
+
+        [SerializeField]
+        private Event _event;
+        
+        public void Invoke(T value) => _event?.Invoke(value);
+        public void AddListener(UnityAction<T> action) => _event.AddListener(action);
+        public void RemoveListener(UnityAction<T> action) => _event.RemoveListener(action);
+    }
+    
     [CreateAssetMenu(fileName = "ScriptableEvent", menuName = "OpenUtility/Scriptable Event/Void")]
     public class ScriptableEvent : ScriptableObject
     {
